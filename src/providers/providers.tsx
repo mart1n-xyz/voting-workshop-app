50af8e32d74ed7c461ce92cf0f528dea09d13141
mart1n <20109376+mart1n-xyz@users.noreply.github.com>
2025-11-03 14:47:21 +0100

    default to SN

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { PrivyProvider } from "@privy-io/react-auth";
+import { statusNetworkSepolia } from "viem/chains";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -8,6 +9,8 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
       clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID!}
       config={{
+        defaultChain: statusNetworkSepolia,
+        supportedChains: [statusNetworkSepolia],
         embeddedWallets: {
           ethereum: {
             createOnLogin: "users-without-wallets",
